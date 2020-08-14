d2c23661944be43cf860ae6e5cd0247e1a61d68e
unknown <C:\MyPrograms\TheBatPortable\Data\Mail>
2020-08-14 17:09:39 +0300

    cors fix

--- src/services/covid.service.js
+++ src/services/covid.service.js
@@ -1,13 +1,13 @@
 export default class covidService {
-  
+
   constructor() {
     this._apiBase = 'http://localhost:3000';
   }
 
   async getStats(url = '/stats') {
-    // const res = await fetch(`${this._apiBase}${url}`);
-    // return await res.json();
-    return await "[{ deahts: 14 }]"
+    const res = await fetch(`${this._apiBase}${url}`);
+    return await res.json();
+    // return await "[{ deahts: 14 }]"
   }
-  
+
 }
