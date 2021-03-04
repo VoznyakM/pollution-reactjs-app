0d2250bc8d4a595e2521b18bd8a01dfd9480f9f3
Naz <voznyaknazar@gmail.com>
2021-03-04 15:10:27 +0200

    social media list implemented

--- src/components/sociallist/sociallist.js
+++ src/components/sociallist/sociallist.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -8,6 +7,8 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Button from "@material-ui/core/Button";
+import React, { useState, useEffect } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,146 +21,75 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 export default function AlignItemsList() {
   const classes = useStyles();
+  const [state, setState] = useState([]);
+
+  const fetchData = async () => {        
+    const response = await fetch(
+      process.env.REACT_APP_BACKEND_API + '/report'
+    );
+    const areas = await response.json();
+    setState({  loading: true,
+                volume_map_data:                       
+                    areas.map(vol => ({
+                      id: vol.id,
+                      description: vol.description,
+                      address: vol.address,
+                      author: vol.author,
+                      avatar: vol.avatar,
+                      source: vol.source,
+                      orig_date: vol.orig_date
+                    }))   
+    });
+  }
 
   const onClick = () => {
     return alert('Зафіксовано в базі');
-  };  
+  };
+
+  useEffect(() => {
+    fetchData();   
+  }, []);
+
+  const rows = state.volume_map_data;
+
+  if (!state.loading) {
+      return ( <CircularProgress /> );
+  }
+
   return (
-    <List className={classes.root}>
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="https://znaj.ua/crops/38946a/620x0/1/0/2020/12/22/jIAi2c7pj4xipth8BrBoiKbsD8P9xa89dAtvic7d.jpeg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Twitter, 3 Feb 2019"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                NGO.DONETSK.UA
-              </Typography>
-              {" — У Маріуполі підпал листя коштуватиме 1700 грн http://bit.ly/ccUsj3"}
-            </React.Fragment>
-          }
-        />
-        <Button onClick={onClick}>Зафіксувати</Button>
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Travis Howard" src="https://abs.twimg.com/errors/logo46x38.png" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Twitter, 10 Nov 2015"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Stanislavske.TV 
-              </Typography>
-              {" — В Івано-Франківську за підпал листя за тиждень склали 6 адміністративних протоколів"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Facebook, 28 Oct 2020"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                 Патрульна поліція Краматорська та Слов'янська
-              </Typography>
-              {' — В Івано-Франківську за підпал листя за тиждень склали 6 адміністративних протоколів'}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="https://znaj.ua/crops/38946a/620x0/1/0/2020/12/22/jIAi2c7pj4xipth8BrBoiKbsD8P9xa89dAtvic7d.jpeg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Twitter, 3 Feb 2019"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                NGO.DONETSK.UA
-              </Typography>
-              {" — У Маріуполі підпал листя коштуватиме 1700 грн http://bit.ly/ccUsj3"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Twitter, 10 Nov 2015"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Stanislavske.TV 
-              </Typography>
-              {" — В Івано-Франківську за підпал листя за тиждень склали 6 адміністративних протоколів"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Facebook, 28 Oct 2020"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                 Патрульна поліція Краматорська та Слов'янська
-              </Typography>
-              {' — В Івано-Франківську за підпал листя за тиждень склали 6 адміністративних протоколів'}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-    </List>
-  );
+  <List className={classes.root}>
+
+  {rows.map(row => {
+          return (
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt={row.author} src={row.avatar} />
+            </ListItemAvatar>
+            <ListItemText
+              primary={row.orig_date}
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                  >
+                    {row.author}
+                  </Typography>
+                  " — {row.description}"
+                </React.Fragment>
+              }
+            />
+            <Button onClick={onClick}>Зафіксувати</Button>
+          </ListItem>
+          // <Divider variant="inset" component="li" />          
+          );
+        })}
+
+  </List>);
+
 }
